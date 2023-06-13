b86684a7f9e9cc5499a2217bcbfd5df892b55e05
stoumposs <136464284+stoumposs@users.noreply.github.com>
2023-06-13 03:30:25 -0900

    Update 01-simple-bot.js

--- scripts/01-simple-bot.js
+++ scripts/01-simple-bot.js
@@ -2,8 +2,8 @@ const mineflayer = require('mineflayer');
 
 // Setup bot connection
 const bot = mineflayer.createBot({
-  host: 'localhost',
-  port: '12345',
-  username: "Hello_world",
-  version: '1.8.9'
+  host: 'parifaetokarimou.aternos.me',
+  port: '45325',
+  username: "WELCOME",
+  version: '1.19.4'
 });
